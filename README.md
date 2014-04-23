40fdbda3ddb61cbbe971f26bf1c46ed133f4f79e
Dmitry Korotovsky <rmpic30@gmail.com>
2014-04-23 12:34:59 +0400

    Fix nested softdeleteable

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Under construction
+
+
+     * That nested case will be corrected and fully supported
+     * {% softdeleteable %}
+     *    <span>
+     *       {% softdeleteable 'Acme\Bundle\CoreBundle\Entity\Foo' %}
+     *           {% softdeleteable %}
+     *               {% softdeleteable 'Acme\Bundle\CoreBundle\Entity\Bar' %}
+     *                   {{ object.owner.fullName }}
+     *               {% endsoftdeleteable %}
+     *           {% endsoftdeleteable %}
+     *       {% endsoftdeleteable %}
+     *    </span>
+     * {% endsoftdeleteable %}
--- src/Krtv/Bundle/TwigSoftdeleteableBundle/Twig/Extension/TwigSoftdeleteableExtension.php
+++ src/Krtv/Bundle/TwigSoftdeleteableBundle/Twig/Extension/TwigSoftdeleteableExtension.php
@@ -3,6 +3,7 @@
 namespace Krtv\Bundle\TwigSoftdeleteableBundle\Twig\Extension;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\SoftDeleteable\Filter\SoftDeleteableFilter;
 use Krtv\Bundle\TwigSoftdeleteableBundle\Twig\TokenParser\TwigSoftdeleteableTokenParser;
 
 /**
@@ -18,31 +19,84 @@ class TwigSoftdeleteableExtension extends \Twig_Extension
     protected $entityManager;
 
     /**
+     * @var \SplStack
+     */
+    protected $classStack;
+
+    /**
      * @param EntityManagerInterface $entityManager
      */
     public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->classStack = new \SplStack();
         $this->entityManager = $entityManager;
     }
 
     /**
      * @param null $class
-     * @return bool
+     * @return void
      */
     public function disable($class = null)
     {
-        return true;
-        // Disable soft deleteable
+        if ($class !== null) {
+            $this->classStack->push($class);
+        }
+
+        if ($this->entityManager->getFilters()->isEnabled('softdeleteable')) {
+            if ($class !== null) {
+                /** @var SoftDeleteableFilter $filter */
+                $filter = $this->entityManager->getFilters()->getFilter('softdeleteable');
+                $filter->disableForEntity($class);
+            } else {
+                $this->entityManager->getFilters()->disable('softdeleteable');
+            }
+        }
     }
 
     /**
+     * That nested case will be corrected and fully supported
+     * {% softdeleteable %}
+     *    <span>
+     *       {% softdeleteable 'Acme\Bundle\CoreBundle\Entity\Foo' %}
+     *           {% softdeleteable %}
+     *               {% softdeleteable 'Acme\Bundle\CoreBundle\Entity\Bar' %}
+     *                   {{ object.owner.fullName }}
+     *               {% endsoftdeleteable %}
+     *           {% endsoftdeleteable %}
+     *       {% endsoftdeleteable %}
+     *    </span>
+     * {% endsoftdeleteable %}
      * @param null $class
      * @return bool
      */
     public function enable($class = null)
     {
-        return true;
-        // Disable soft deleteable
+        if (!$this->entityManager->getFilters()->isEnabled('softdeleteable')) {
+            if ($class !== null) {
+                // Nested tags case.
+                // {% softdeleteable 'FQCN' %} incide {% softdeleteable %}
+                // So, just pop classes stack
+                $this->classStack->pop();
+            } else {
+                // When we enable globally we need to restore disabled entities
+                $this->entityManager->getFilters()->enable('softdeleteable');
+
+                // Populate from stack of disabled entities
+                /** @var SoftDeleteableFilter $filter */
+                $filter = $this->entityManager->getFilters()->getFilter('softdeleteable');
+                foreach ($this->classStack as $class) {
+                    $filter->disableForEntity($class);
+                }
+            }
+        } else {
+            if ($class !== null) {
+                /** @var SoftDeleteableFilter $filter */
+                $filter = $this->entityManager->getFilters()->getFilter('softdeleteable');
+                $filter->enableForEntity($class);
+
+                $this->classStack->pop();
+            }
+        }
     }
 
     /**
